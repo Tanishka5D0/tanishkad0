8869e6647ddde871369a69049e1eba40d18da034
Pitla Tanishka <pitlatanishka@gmail.com>
2024-12-14 10:33:27 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,8 +1,5 @@
-#tanishka
-# This program adds two numbers
-num1 = 1.5
-num2 = 6.3
-# Add two numbers
-sum = num1 + num2
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+class Test{
+  public static void main(string[] args){
+     System.out.println("Hello World");
+   }
+}
